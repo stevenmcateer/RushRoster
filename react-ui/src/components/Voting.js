60f225f9f25d0b739aadca97bcb8c8c261ade9d7
Steven McAteer <smcateer@wpi.edu>
2018-10-07 20:17:15 -0400

    voting rounds

--- react-ui/src/components/Voting.js
+++ react-ui/src/components/Voting.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import AddRushee from "./AddRushee";
 import Container from "semantic-ui-react/dist/commonjs/elements/Container/Container";
 import Card from "semantic-ui-react/dist/commonjs/views/Card/Card";
 import Button from "semantic-ui-react/dist/commonjs/elements/Button/Button";
-import {getPnms} from "../scripts";
-import {Grid} from "semantic-ui-react/dist/commonjs/collections/Grid/Grid";
+import Grid from "semantic-ui-react/dist/commonjs/collections/Grid/Grid";
 import Rushee from "./Rushee";
 
 
-
-export default class Voting extends Component{
+export default class Voting extends Component {
 
     static defaultProps = {
         user: {
@@ -20,48 +18,104 @@ export default class Voting extends Component{
         },
     }
 
-    constructor(props){
-        super(props);
+    constructor(props) {
+        super(props)
         this.state = {
             rows: []
         }
-
-        // fetch all PNMs from db into state
-        getPnms().then(res => {
-            this.setState({rows: res.rows})
-        })
+        this.refreshData = this.refreshData.bind(this)
+        this.refreshData()
     }
 
-    render(){
+    // fetch all bids from db into state, forcing re-render
+    refreshData() {
+        // getBids().then(res => {
+        //     this.setState({rows: JSON.parse(res.getBody())})
+        // })
+    }
+
+    render() {
         return (
             <Container id={"ContainerDiv"} className={"ContainerDiv"}>
-                {this.state.rows.map(row => {
+                <Grid doubling columns={3} padded>
+                    <Grid.Column>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>Round 1</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>Monday, Nov. 6th</span>
+                                </Card.Meta>
+                            </Card.Content>
+                            <Card.Content>
+                                <div role='list' className='ui list'>
+                                    <div role='listitem' className='item'>
+                                        <img
+                                            src='https://react.semantic-ui.com/images/avatar/small/rachel.png'
+                                            className='ui avatar image'
+                                        />
+                                        <div className='content'>
+                                            <a className='header'>Bidee</a>
+                                            <div className='description'>
 
-                })}
-                <Card>
-                    <Card.Content>
-                        <Card.Header>Round 1</Card.Header>
-                        <Card.Meta>
-                            <span className='date'>Monday, Nov. 6th</span>
-                        </Card.Meta>
-                    </Card.Content>
-                    <Card.Content>
-                        <div role='list' className='ui list'>
-                            <div role='listitem' className='item'>
-                                <img
-                                    src='https://react.semantic-ui.com/images/avatar/small/rachel.png'
-                                    className='ui avatar image'
-                                />
-                                <div className='content'>
-                                    <a className='header'>Bidee</a>
-                                    <div className='description'>
-
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </Card.Content>
-                </Card>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>Round 2</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>Monday, Nov. 6th</span>
+                                </Card.Meta>
+                            </Card.Content>
+                            <Card.Content>
+                                <div role='list' className='ui list'>
+                                    <div role='listitem' className='item'>
+                                        <img
+                                            src='https://react.semantic-ui.com/images/avatar/small/rachel.png'
+                                            className='ui avatar image'
+                                        />
+                                        <div className='content'>
+                                            <a className='header'>Bidee</a>
+                                            <div className='description'>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>Round 3</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>Monday, Nov. 6th</span>
+                                </Card.Meta>
+                            </Card.Content>
+                            <Card.Content>
+                                <div role='list' className='ui list'>
+                                    <div role='listitem' className='item'>
+                                        <img
+                                            src='https://react.semantic-ui.com/images/avatar/small/rachel.png'
+                                            className='ui avatar image'
+                                        />
+                                        <div className='content'>
+                                            <a className='header'>Bidee</a>
+                                            <div className='description'>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                </Grid>
                 <Button positive>Save Round Results</Button>
             </Container>
         )
