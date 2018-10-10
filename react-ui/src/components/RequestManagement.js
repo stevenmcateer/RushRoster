c812f6e10cebfd337392c08f48f75be5eeb0a767
Steven McAteer <smcateer@wpi.edu>
2018-10-10 19:21:35 -0400

    yeah idk

--- react-ui/src/components/RequestManagement.js
+++ react-ui/src/components/RequestManagement.js
@@ -4,6 +4,7 @@ import Grid from "semantic-ui-react/dist/commonjs/collections/Grid/Grid";
 import {getAll, getEditedPNMs} from "../scripts";
 import Rushee from "./Rushee";
 import Request from "./Request";
+import Async from "react-promise";
 
 export default class RequestManagement extends Component {
 
@@ -22,40 +23,47 @@ export default class RequestManagement extends Component {
             edited: [],
             rushees: []
         }
+        this.child = React.createRef();
         this.refreshData = this.refreshData.bind(this)
         this.refreshData()
     }
 
     // fetch all PNMs from db into state, forcing re-render
     refreshData() {
+        //this.setState({loading: true})
         console.log("Refreshing data")
-
         getEditedPNMs().then(res => {
-            console.log("edits")
             console.log(JSON.parse(res.getBody()))
             this.setState({edited: JSON.parse(res.getBody())})
-        })
-        getAll().then(res => {
-            console.log("all rushees")
-            let rushees = JSON.parse(res.getBody())
-            console.log("type: " + typeof rushees)
-            console.log("rushees: " + rushees)
-            console.log("stringify: " + JSON.stringify(rushees))
 
-            this.setState({rushees: rushees}, this.createMerged())
+            getAll().then(res => {
+                console.log(JSON.parse(res.getBody()))
+                this.setState({rushees: JSON.parse(res.getBody())})
+
+            })
         })
+
+    }
+
+    callChild() {
+        this.child.current.createMerged();
     }
 
 
     render() {
+        console.log("data ready")
+        console.log(this.state.edited)
+        console.log(this.state.rushees)
+
         return (
             <Container>
                 <h3>Rushee Edit Requests</h3>
-                {this.state.edited.map((edit) => {
+                {this.state.edited.map(edit => {
                     return (
-                        <Request refreshData={this.refreshData} edited={this.state.edited} rushees={this.state.rushees}/>
+                        <Request callBack={this.callChild} edited={this.state.edited} rushees={this.state.rushees} />
                     )
-                })}
+                })
+                }
             </Container>
         )
     }
